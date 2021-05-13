7d16e5cd04ba02fea04ec4f78b7149d01f79b359
Prasanna Selvaraj <prasannas@twitter.com>
2021-05-13 14:32:05 -0400

    health

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const search = require('./controllers/search')
 
 const app = express();
-const port = 4040;
+const port = process.env.PORT || 4040;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
